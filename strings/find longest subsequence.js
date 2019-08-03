a4f3b3c677b95ad0c4ed019ea953ed33c9fa2b22
Ilya Novohatskyi <1llyanovohatskyy@gmail.com>
2019-08-03 20:45:51 +0300

    Create find longest subsequence.js

---
+++ strings/find longest subsequence.js
@@ -0,0 +1,22 @@
+const S = "abppplee";
+const D = ["able", "ale", "apple", "bale", "kangaroo"];
+
+const findLongestSubsequence = (s, d) => {
+  let index, temp;
+  let maxLen = 0;
+  for(let i = 0; i < d.length; i++) {
+    temp = s;
+    for(let j = 0, len = d[i].length; j < len; j++) {
+      index = temp.indexOf(d[i][j]);
+      if(index == -1) break;
+      temp = temp.slice(index+1);
+      if(j == len-1) {
+        maxLen = maxLen > len ? maxLen : len;
+      }
+    }
+  }
+
+  return maxLen;
+}
+
+console.log(findLongestSubsequence(S, D) == 5)
